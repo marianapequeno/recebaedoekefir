43aaf05aef8f4884be0b1a62083e89bb11a01162
Mariana Pequeno <pequeno.mariana@gmail.com>
2022-07-30 11:05:58 -0300

    Finalização

--- src/routes/pages/Comunidade/Comunidade.jsx
+++ src/routes/pages/Comunidade/Comunidade.jsx
@@ -1,10 +1,74 @@
-import React from 'react'
+import React from 'react';
+import './comunidade.css';
+import { GoogleMap, useJsApiLoader } from '@react-google-maps/api';
 
 function Comunidade() {
+  const { isLoaded } = useJsApiLoader({
+    id: 'google-map-script',
+    googleMapsApiKey: "AIzaSyBuWY6bhoKldVwfOCN-PV6sYF9an1_6Z34"
+  })
+
   return (
-    <div>
-      Comunidade
-    </div>
+    <main className="main">
+      <section className="section-map">
+        <h2 className="title-2">Encontre um doador na sua região</h2>
+        {
+          isLoaded ? (
+            <GoogleMap
+              mapContainerStyle={{width: "100%", height: "100%", margin: "auto"}}
+              center={{
+                lat: -23.549782,
+                lng: -46.633925
+              }}
+              zoom={15}
+            >
+            </GoogleMap>
+        ) : <></>
+        }
+      </section>
+
+      <section className="section-form">
+        <h2 className="title-1">Cadastre-se em para doar ou receber</h2>
+        <p>Caso não tenha achado alguém na sua região cadastre-se abaixo: </p>
+        <form 
+          action="https://docs.google.com/forms/u/0/d/e/1FAIpQLSfK8FbK372inolVMn4ktUI7TebDKpFbWwReu7vnTzP-8UdCSA/formResponse"
+          name="contato"
+          id="contato"
+        >
+
+          <label htmlFor="nome" className="label">Nome</label>
+          <input 
+            type="text" 
+            name="entry.519160718" 
+            id="nome" 
+            className="input" 
+            placeholder="Seu nome" 
+            required
+          />
+
+          <label htmlFor="email" className="label">E-mail</label>
+          <input 
+            type="email" 
+            name="entry.1335459650" 
+            id="email" 
+            className="input" 
+            placeholder="Seu e-mail" 
+            required
+          />
+
+          <label htmlFor="estado" className="label">Estado</label>
+          <input type="text" name="entry.753205699" id="estado" className="input" placeholder="Digite seu estado" required/>
+
+          <label htmlFor="cidade" className="label">Cidade</label>
+          <input type="text" name="entry.1089904732" id="cidade" className="input" placeholder="Digite o nome da sua cidade" required/>
+
+          <label htmlFor="bairro" className="label">Bairro</label>
+          <input type="text" name="entry.864223599" id="bairro" className="input" placeholder="Digite o nome do seu bairro" required/>
+
+          <button type="submit" className="btn">Enviar</button>
+        </form>
+      </section>
+    </main>
   )
 }
 

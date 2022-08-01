0fe101cf8c917624dbe67794949a6c6bc4036b4a
Mariana Pequeno <pequeno.mariana@gmail.com>
2022-08-01 12:15:22 -0300

    Ajustes de estilo e na function do menu mobile

--- src/routes/components/Menu/Menu.jsx
+++ src/routes/components/Menu/Menu.jsx
@@ -3,9 +3,24 @@ import './menu.css'
 import image from '../../../assets/logo.png'
 
 function Menu() {
-  function handleClick(){
+  function handleClick(event){
     const menuList = document.querySelector(".menu-list-mobile");
+    const barMobile1 = document.querySelector(".bar-mobile-1");
+    const barMobile2 = document.querySelector(".bar-mobile-2");
+    const barMobile3 = document.querySelector(".bar-mobile-3");
+   
     menuList.classList.toggle("inativo");
+    menuList.classList.toggle("active");
+    barMobile1.classList.toggle("active");
+    barMobile3.classList.toggle("active");
+
+    if(!menuList.classList.contains("inativo")){
+      barMobile2.style.display = "none";
+      event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    }else{
+      barMobile2.style.display = "block";
+      event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    }
   }
 
   return (
@@ -26,15 +41,15 @@ function Menu() {
          <img src={image} alt="Logotipo Comunidade Kefir" className="link-mobile menu-img"/>
          <p className="paragraph">Doe Kefir</p>
          <div className="menu-btn-mobile" onClick={handleClick}>
-          <span className="bar-mobile"></span>
-          <span className="bar-mobile"></span>
-          <span className="bar-mobile"></span>
+          <span className="bar-mobile bar-mobile-1"></span>
+          <span className="bar-mobile bar-mobile-2"></span>
+          <span className="bar-mobile bar-mobile-3"></span>
          </div>
          <ul className="menu-list-mobile inativo">
-          <li><Link className="link" to="/">Home</Link></li>
-          <li><Link className="link" to="/sobre">Sobre</Link></li>
-          <li><Link className="link" to="/comunidade">Comunidade</Link></li>
-          <li><Link className="link" to="/contato">Contato</Link></li>
+          <li><Link className="link mobile-link" to="/">Home</Link></li>
+          <li><Link className="link mobile-link" to="/sobre">Sobre</Link></li>
+          <li><Link className="link mobile-link" to="/comunidade">Comunidade</Link></li>
+          <li><Link className="link mobile-link" to="/contato">Contato</Link></li>
          </ul>
       </div>
     </nav>
